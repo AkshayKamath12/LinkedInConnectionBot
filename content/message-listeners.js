216714958bee1d484c1694452f5a3f174e2c8137
Akshay Kamath <akkamath@ucsc.edu>
2024-06-24 16:02:47 -0700

    testing arr passing

--- content/message-listeners.js
+++ content/message-listeners.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.action === "startConnecting") {
     arr = message.message;
+    concole.log(arr);
     startConnecting(message);
   }
 });
