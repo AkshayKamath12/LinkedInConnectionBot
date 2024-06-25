8561bcdde30d0fc121994a9aca4882141c2b51ea
Akshay Kamath <akkamath@ucsc.edu>
2024-06-24 19:08:48 -0700

    checking urls

--- content/linkedin-connector.js
+++ content/linkedin-connector.js
@@ -9,32 +9,17 @@ function connect(url){
     window.location.href = url;
     
 }
-async function startConnecting(connectionsArray) {
-    total = connectionsArray.length
-    console.log(total);
-    for(i = 0; i < connectionsArray.length; i++){
-        connection = connectionsArray[i];
-        if(connection.length === 0){
-            console.log("error: empty row in csv");
-            continue;
-        }
-        url = connection[0];
-function sendProgressPercentage(progressPercentage) {
-  chrome.runtime.sendMessage({
-    action: "progressPercentage",
-    message: progressPercentage,
-  });
-}
+
 function connect(url){
-    
-    window.location.href = url;
-    
+    window.location.href = url;   
 }
+
 async function startConnecting(connectionsArray) {
     total = connectionsArray.length
     console.log(total);
     for(i = 0; i < connectionsArray.length; i++){
         connection = connectionsArray[i];
+        console.log(connection);
         if(connection.length === 0){
             console.log("error: empty row in csv");
             continue;
@@ -44,9 +29,19 @@ async function startConnecting(connectionsArray) {
         elem.href = url;
         elem.setAttribute("type", "hidden");
         await new Promise((resolve) => setTimeout(resolve, 750));
-        elem.click();
-        await new Promise((resolve) => setTimeout(resolve, 750));
-        await elem.click();
+        if(isValidUrl(url)){
+            await elem.click();
+        }
         sendProgressPercentage(Math.round((i+1) / total) * 100);
     }
+}
+
+
+function isValidUrl(url){
+    try { 
+      	return Boolean(new URL(urlString)); 
+      }
+      catch(e){ 
+      	return false; 
+      }
 }
