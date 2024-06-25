60c252fe14af2bceeed99c2ac1ad08be927c2c37
Akshay Kamath <akkamath@ucsc.edu>
2024-06-24 17:52:11 -0700

    iterating through csv array and navigating to each url

--- content/linkedin-connector.js
+++ content/linkedin-connector.js
@@ -7,10 +7,26 @@ function sendProgressPercentage(progressPercentage) {
   });
 }
 
+function connect(url){
+    window.location.href = url;
+    
+}
+
 
 async function startConnecting(connectionsArray) {
-  console.log(connectionsArray.length);
-
+    total = connectionsArray.length
+    console.log(total);
+    for(i = 0; i < connectionsArray.length; i++){
+        connection = connectionsArray[i];
+        if(connection.length === 0){
+            console.log("error: empty row in csv");
+            continue;
+        }
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        connect(connection[0]);
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        sendProgressPercentage(Math.round((i / total) * 100));
+    }
 }
 
 
