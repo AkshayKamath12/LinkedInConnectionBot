686e5b6e4efb5252a881c7568d5f96d78e650285
Akshay Kamath <akkamath@ucsc.edu>
2024-06-26 14:17:15 -0700

    finalizing changes

--- background/background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === "redirect") {
-    chrome.tabs.query({currentWindow: true, active:true}, function (tab) {
-      chrome.tabs.update(tab.id, {url: message.url});
-    });
-  }
-});
